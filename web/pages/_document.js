6a917364df449820d4fbcf65662c37c045eb7e41
RONNY FREITES <ronnyangelo.freites@gmail.com>
2020-07-19 14:28:53 -0600

    add contact script

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -13,7 +13,26 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang={this.props.lang || "en"}>
-        <Head />
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function () {
+                var options = {
+                    whatsapp: "+50689438510", // WhatsApp number
+                    call: "+50689438510", // Call phone number
+                    call_to_action: "Llama o escribenos al Whatsapp.", // Call to action
+                    button_color: "#000000", // Color of button
+                    position: "right", // Position may be 'right' or 'left'
+                    order: "whatsapp,call", // Order of buttons
+                };
+                var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+                var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+                s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+                var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+            })();`,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
