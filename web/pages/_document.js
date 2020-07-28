0e81d3a8c5537b7409d72b8a77cbc0a4c2782324
RONNY FREITES <ronnyangelo.freites@gmail.com>
2020-07-28 17:41:51 -0600

    add sitemap

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -68,6 +68,7 @@ export default class MyDocument extends Document {
             href='/static/favicon/favicon-16x16.png'
           />
           <link rel='manifest' href='/static/favicon/manifest.json' />
+          <link rel='sitemap' type='application/xml' title='Sitemap' href='/static/sitemap/sitemap.xml' />
           <meta name='msapplication-TileColor' content='#000000' />
           <meta name='msapplication-TileImage' content='/static/favicon/ms-icon-144x144.png' />
           <meta name='theme-color' content='#000000' />
